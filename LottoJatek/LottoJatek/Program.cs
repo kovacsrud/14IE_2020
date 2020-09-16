9e091958e2dc2ff8656ba020ed02fe29c4bd865a
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-16 17:22:01 +0200

    Update Program.cs

--- LottoJatek/LottoJatek/Program.cs
+++ LottoJatek/LottoJatek/Program.cs
@@ -10,6 +10,18 @@ namespace LottoJatek
     {
         static void Main(string[] args)
         {
+            // A feladat egy lottójáték elkészítése
+            // A felhasználótól kérjük be, hogy hány
+            // számmal akarunk játszani, illetve mennyiből
+            // akarunk sorsolni. Ezután kérjük be a
+            // felhasználó tippjeit. A tippeknek egyedinek
+            // kell lenniük, és a megadott számtartományba
+            // esniük pl. 1-90
+            // A tippek bekérése után sorsoljunk nyerőszámokat
+            // Nem lehet két egyforma nyerőszám.
+            // A sorsolás után állapítsuk meg, hány találat volt.
+
+
 
             Console.ReadKey();
         }
