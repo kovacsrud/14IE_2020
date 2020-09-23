a8259207d2a4052621d095450ca20d81aef38125
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-23 16:06:15 +0200

    OOP állat

---
+++ AllatOOP/AllatOOP/Allat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllatOOP
+{
+    public class Allat
+    {
+        //adattagok
+        private string nev;
+        private int suly;
+
+        public void Eszik()
+        {
+            Console.WriteLine("Az állat eszik");
+        }
+
+        public void Mozog()
+        {
+            Console.WriteLine("Az állat mozog");
+        }
+
+        public void Alszik()
+        {
+            Console.WriteLine("AZ állat alszik");
+        }
+
+        public void SetNev(string nev)
+        {
+            this.nev = nev;
+        }
+
+        public string GetNev()
+        {
+            return nev;
+        }
+        
+    }
+}
---
+++ AllatOOP/AllatOOP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllatOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Allat allat = new Allat();
+            Allat masikAllat = new Allat();
+
+            allat.SetNev("Ubul");
+
+
+            allat.Eszik();
+            allat.Mozog();
+            allat.Alszik();
+            Console.WriteLine(allat.GetNev());
+
+            masikAllat.Mozog();
+
+
+            Console.ReadKey();
+        }
+    }
+}
