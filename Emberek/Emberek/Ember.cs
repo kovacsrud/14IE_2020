88aaa9d0a9b2cc4889c977283c45986b673d5f66
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-23 17:32:39 +0200

    Emberek start

---
+++ Emberek/Emberek/Ember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emberek
+{
+    public class Ember
+    {
+        public string Vezeteknev { get; set; }
+        public string Keresztnev { get; set; }
+        public int Magassag { get; set; }
+        public string Szulhely { get; set; }
+        public int SzulEv { get; set; }
+    }
+}
