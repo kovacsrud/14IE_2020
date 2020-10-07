ff6a53c1dcdd4ee6caedee31d90f34a561e7a7a3
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-07 18:09:27 +0200

    Update Program.cs

--- Idojaras/Idojaras/Program.cs
+++ Idojaras/Idojaras/Program.cs
@@ -20,6 +20,22 @@ namespace Idojaras
 
             var ev2007 = adatok.IdojarasAdatLista.FindAll(x => x.Ev == 2007);
 
+            //foreach (var i in ev2007)
+            //{
+            //    Console.WriteLine($"{i.Ev}.{i.Honap}.{i.Nap}");
+            //}
+
+            //Mennyi volt az év átlaghőmérséklete?
+            var atlagho2007 = ev2007.Average(x=>x.Homerseklet);
+            Console.WriteLine(atlagho2007);
+
+            Console.WriteLine(ev2007.FindAll(x=>x.Honap==3).Average(x=>x.Homerseklet));
+
+            //Melyik napon volt a legmelegebb 2007_ben?
+
+            var legmelegebb = ev2007.Find(x=>x.Homerseklet==ev2007.Max(y=>y.Homerseklet));
+
+            Console.WriteLine($"{legmelegebb.Honap}.{legmelegebb.Nap}-{legmelegebb.Ora},{legmelegebb.Homerseklet}");
 
             Console.ReadKey();
         }
