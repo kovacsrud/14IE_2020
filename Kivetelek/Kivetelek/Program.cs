33ce3de4b9709aa0963f0ad55d05d28e29a6de89
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-07 15:02:20 +0200

    Kivételkezelés példák

--- Kivetelek/Kivetelek/Program.cs
+++ Kivetelek/Kivetelek/Program.cs
@@ -10,8 +10,47 @@ namespace Kivetelek
     {
         static void Main(string[] args)
         {
+            //Kivétel: a program futását megállító  esemény, melynek
+            //kezelésére szükség van
+            try
+            {
+                int a = 10;
+                int b = 0;
+                try
+                {
+                    int c = a / b;
+                    Console.WriteLine(c);
+                }
+                catch (DivideByZeroException ex)
+                {
+                   Console.WriteLine("Nem lehet b 0, mert 0-val nem lehet osztani.");
+                }
+                
+
+                throw new ArithmeticException();
+
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Null hiba");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Hibás érték");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);                
+            }
+            finally
+            {
+                //Takarítás
+                //Lefoglalt erőforrások felszabadítása
+                Console.WriteLine("Ez a kód mindig lefut");
+            }
 
 
+            Console.WriteLine("try-catch utáni programrész");
             Console.ReadKey();
         }
     }
