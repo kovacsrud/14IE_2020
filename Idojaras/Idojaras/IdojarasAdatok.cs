02b9d5e7fe8efd01a02ba6a838e18d0541dec5f7
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-07 17:48:23 +0200

    Lekérdezések az adatokból

---
+++ Idojaras/Idojaras/IdojarasAdatok.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idojaras
+{
+    public class IdojarasAdatok
+    {
+        private List<IdojarasAdat> idojarasadatlista;
+        public List<IdojarasAdat> IdojarasAdatLista {
+
+            get { return idojarasadatlista; }
+                 
+        }
+
+        public IdojarasAdatok(string fajl,bool isOszlopnevek,char elvalaszto)
+        {
+            idojarasadatlista = new List<IdojarasAdat>();
+            try
+            {
+                var sorok = File.ReadAllLines(fajl,Encoding.Default);
+                var kezdosor = 0;
+                if (isOszlopnevek)
+                {
+                    kezdosor = 1;
+                }
+                for (int i = kezdosor; i < sorok.Length; i++)
+                {
+                    var e = sorok[i].Split(elvalaszto);
+                    idojarasadatlista.Add(
+                        new IdojarasAdat
+                        {
+                            Ev=Convert.ToInt32(e[0]),
+                            Honap= Convert.ToInt32(e[1]),
+                            Nap= Convert.ToInt32(e[2]),
+                            Ora=Convert.ToInt32(e[3]),
+                            Homerseklet=Convert.ToDouble(e[4]),
+                            Szelsebesseg= Convert.ToDouble(e[5]),
+                            Paratartalom= Convert.ToDouble(e[6])
+                        }
+                        
+                        );
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+        }
+
+    }
+}
--- Idojaras/Idojaras/Program.cs
+++ Idojaras/Idojaras/Program.cs
@@ -10,6 +10,17 @@ namespace Idojaras
     {
         static void Main(string[] args)
         {
+            //A főprogramban a fájlkezelés most tilos.
+            IdojarasAdatok adatok = new IdojarasAdatok("idojaras.csv", true, ';');
+
+            Console.WriteLine(adatok.IdojarasAdatLista.Count);
+
+            Console.WriteLine(adatok.IdojarasAdatLista.Max(x=>x.Ev));
+            Console.WriteLine(adatok.IdojarasAdatLista.Min(x => x.Ev));
+
+            var ev2007 = adatok.IdojarasAdatLista.FindAll(x => x.Ev == 2007);
+
+
             Console.ReadKey();
         }
     }
