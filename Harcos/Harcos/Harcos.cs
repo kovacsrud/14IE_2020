68d8e7960080bed036a06e41c064cc2c013f1185
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-14 16:11:27 +0200

    Harcos feladat start

---
+++ Harcos/Harcos/Harcos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harcos
+{
+    public class Harcos
+    {
+        private string nev;
+        private int eletero;
+        private int harciero;
+
+       
+
+        public int Eletero
+        {
+            get
+            {
+                return eletero;
+            }
+
+            set
+            {
+                eletero = value;
+            }
+        }
+
+        public int Harciero
+        {
+            get
+            {
+                return harciero;
+            }
+                      
+        }
+
+        public Harcos(string nev, int eletero, int harciero)
+        {
+            this.nev = nev;
+            this.eletero = eletero;
+            this.harciero = harciero;
+        }
+
+
+    }
+}
