ef68bec7485a3c36c1354741e95b5e2276849e00
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-21 17:58:09 +0200

    Idomeres

---
+++ WpfIdomeres/WpfIdomeres/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfIdomeres
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Stopwatch stopper;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            stopper = new Stopwatch();
+            buttonStartStop.Content = "Start";
+           
+        }
+
+        private void buttonStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (stopper.IsRunning)
+            {
+                stopper.Stop();
+                labelElteltIdo.Content = stopper.ElapsedMilliseconds;
+                stopper.Reset();
+                buttonStartStop.Content = "Start";
+            } else
+            {
+                stopper.Start();
+                buttonStartStop.Content = "Stop";
+            }
+        }
+    }
+}
