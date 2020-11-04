dae3f6c3780c316b8cc768ca764c1c31fda244fc
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-04 18:16:49 +0100

    Képtároló osztály

---
+++ WpfKeptarolo/WpfKeptarolo/Keptarolo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace WpfKeptarolo
+{
+    public class Keptarolo:StackPanel
+    {
+        BitmapImage bitmap;
+        Image kep;
+        TextBlock fajlinfo;
+
+        public Keptarolo(string fajl)
+        {
+            bitmap = new BitmapImage(new Uri(fajl));
+            kep = new Image();
+            kep.Source = bitmap;
+            fajlinfo = new TextBlock();
+            fajlinfo.Text = fajl;
+            fajlinfo.TextAlignment = TextAlignment.Center;
+            fajlinfo.FontSize = 16;
+            fajlinfo.TextWrapping = TextWrapping.Wrap;
+            this.Children.Add(kep);
+            this.Children.Add(fajlinfo);
+        }
+
+    }
+}
---
+++ WpfKeptarolo/WpfKeptarolo/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKeptarolo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Multiselect = true;
+                if (dialog.ShowDialog()==true)
+                {
+                    var files = dialog.FileNames;
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        Keptarolo kep = new Keptarolo(files[i]);
+                        kep.Width = this.Width/5;
+                        panelKepek.Children.Add(kep);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);                
+            }
+        }
+    }
+}
