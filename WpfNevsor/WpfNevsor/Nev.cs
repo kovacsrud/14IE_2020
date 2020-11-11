bb198a71c19e4291e6ea8153516ced06b92e905e
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-11 15:23:52 +0100

    Nevsor start

---
+++ WpfNevsor/WpfNevsor/Nev.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfNevsor
+{
+    public class Nev
+    {
+        public string Vezeteknev { get; set; }
+        public string Keresztnev { get; set; }
+        public int SzuletesiEv { get; set; }
+        public string Szuletesihely { get; set; }
+    }
+}
