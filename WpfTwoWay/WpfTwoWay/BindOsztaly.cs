f068e20179b847a7d33d5b6cbdc776f500a5d9f3
szajberpiraty <szajberpiraty@gmail.com>
2020-11-18 16:50:54 +0100

    ADatkötés példa

---
+++ WpfTwoWay/WpfTwoWay/BindOsztaly.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTwoWay
+{
+    public class BindOsztaly:INotifyPropertyChanged
+    {
+        private int bindadat;
+
+       
+
+        public int BindAdat { 
+            get { return bindadat; }
+            set
+            {
+                if (value!=bindadat && value>=0 && value<=100)
+                {
+                    bindadat = value;
+                    Prop_Changed("BindAdat");
+                }
+            }
+        }
+        public BindOsztaly(int ertek)
+        {
+            BindAdat = ertek;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Prop_Changed(string propname)
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propname));
+            }
+        }
+    }
+}
