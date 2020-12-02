e0a01161f89d72419c8f67c7568cab3565971b96
szajberpiraty <szajberpiraty@gmail.com>
2020-12-02 16:56:03 +0100

    Kép külön ablakban

--- WpfAPI/WpfAPI/MainWindow.xaml.cs
+++ WpfAPI/WpfAPI/MainWindow.xaml.cs
@@ -98,7 +98,7 @@ namespace WpfAPI
                 stackAdatok.Children.Add(DataTextBlock($"{DateTimeOffset.FromUnixTimeSeconds((long)jsonWeather["dt"]).DateTime}", 20));
                 stackAdatok.Children.Add(DataTextBlock((string)jsonWeather["weather"][0]["main"], 20));
                 stackAdatok.Children.Add(DataTextBlock((string)jw["weather"][0]["description"], 20));
-                netKep.Source = KepFromUrl("https://taszi.hu/kepek/kepkezelo/large/2828.jpg");
+                //netKep.Source = KepFromUrl("https://taszi.hu/kepek/kepkezelo/large/2828.jpg");
             }
             catch (Newtonsoft.Json.JsonReaderException ex)
             {
@@ -109,6 +109,12 @@ namespace WpfAPI
             {
                 MessageBox.Show(ex.Message, "Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+        private void buttonKep_Click(object sender, RoutedEventArgs e)
+        {
+            PicWindow picwindow = new PicWindow(KepFromUrl("https://taszi.hu/kepek/kepkezelo/large/2828.jpg"));
+            picwindow.Show();
         }
     }
 }
