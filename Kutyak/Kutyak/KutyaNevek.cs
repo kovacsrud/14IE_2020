aadf38d6c3a4ac9cc06cfd91bc4dd1f52ef22298
szajberpiraty <szajberpiraty@gmail.com>
2020-12-09 14:46:00 +0100

    Create KutyaNevek.cs

---
+++ Kutyak/Kutyak/KutyaNevek.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutyak
+{
+    public class KutyaNevek
+    {
+        public class Kutyanev
+        {
+            public int Id { get; set; }
+            public string Nev { get; set; }
+            public string EredetiNev { get; set; }
+        }
+
+        private List<Kutyanev> kutyanevek;
+        public List<Kutyanev> Kutyanevek { get { return kutyanevek; }  }
+
+        public KutyaNevek(string fajl)
+        {
+            kutyanevek = new List<Kutyanev>();
+            try
+            {
+                var sorok = File.ReadAllLines(fajl,Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    var e = sorok[i].Split(';');
+                    kutyanevek.Add(
+                        new Kutyanev
+                        {
+                            Id=Convert.ToInt32(e[0]),
+                            Nev=e[1],
+                            EredetiNev=e[2]
+                        }
+                        );
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
