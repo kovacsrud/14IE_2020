49967abee1e53a365a36860116337a506557f811
szajberpiraty <szajberpiraty@gmail.com>
2021-01-06 16:09:55 +0100

    Update FajtaAdapter.cs

--- WpfKutyaAdapter/WpfKutyaAdapter/FajtaAdapter.cs
+++ WpfKutyaAdapter/WpfKutyaAdapter/FajtaAdapter.cs
@@ -1,6 +1,62 @@
-﻿namespace WpfKutyaAdapter
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfKutyaAdapter
 {
-    internal class FajtaAdapter
+    public class FajtaAdapter
     {
+        string connString;
+        DataTable kutyafajtaadatok;
+
+        public DataView Kutyafajtaadatok { get { return kutyafajtaadatok.DefaultView; } }
+        
+        SQLiteDataAdapter adapter;
+        SQLiteConnection conn;
+
+        public FajtaAdapter(string connstring)
+        {
+            connString = connstring;
+            kutyafajtaadatok = new DataTable();
+            conn = new SQLiteConnection(connString);
+            adapter = new SQLiteDataAdapter("", conn);
+
+            adapter.SelectCommand = new SQLiteCommand(conn);
+            adapter.SelectCommand.CommandText = "select * from kutyafajtak";
+
+            adapter.InsertCommand = new SQLiteCommand(conn);
+            adapter.InsertCommand.CommandText = "insert into kutyafajtak (nev,eredetinev)" +
+                "values(@nev,@eredetinev)";
+            adapter.InsertCommand.Parameters.Add("@nev", DbType.String, 0, "nev");
+            adapter.InsertCommand.Parameters.Add("@eredetinev",DbType.String,0,"eredetinev");
+
+            adapter.UpdateCommand = new SQLiteCommand(conn);
+            adapter.UpdateCommand.CommandText = "update kutyafajtak" +
+                " set nev=@nev,eredetinev=@eredetinev " +
+                "where id=@id";
+            adapter.UpdateCommand.Parameters.Add("@nev",DbType.String,0,"nev");
+            adapter.UpdateCommand.Parameters.Add("@eredetinev", DbType.String, 0, "eredetinev");
+            adapter.UpdateCommand.Parameters.Add("@id", DbType.Int32, 0, "id").SourceVersion = DataRowVersion.Original;
+
+            adapter.DeleteCommand = new SQLiteCommand(conn);
+            adapter.DeleteCommand.CommandText = "delete from kutyafajtak where id=@id";
+            adapter.DeleteCommand.Parameters.Add("@id", DbType.Int32, 0, "id").SourceVersion = DataRowVersion.Original;
+
+            adapter.Fill(kutyafajtaadatok);
+
+        }
+
+        public void KutyafajtaUpdate()
+        {
+            adapter.Update(kutyafajtaadatok);
+            kutyafajtaadatok.Clear();
+            adapter.Fill(kutyafajtaadatok);
+
+        }
+
     }
 }
