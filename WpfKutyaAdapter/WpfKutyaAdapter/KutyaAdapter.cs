69ec2767c80e8a290a3cb72d13a1f7e0e5a898f0
szajberpiraty <szajberpiraty@gmail.com>
2021-01-06 15:05:45 +0100

    Adapter készítés

---
+++ WpfKutyaAdapter/WpfKutyaAdapter/KutyaAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfKutyaAdapter
+{
+    public class KutyaAdapter
+    {
+        string connString;
+        NevAdapter nevadapter;
+        FajtaAdapter fajtaadapter;
+        KezelesAdapter kezelesadapter;
+
+        public KutyaAdapter(string connstring)
+        {
+            connString = connstring;
+            nevadapter = new NevAdapter(connString);
+
+        }
+    }
+}
---
+++ WpfKutyaAdapter/WpfKutyaAdapter/NevAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfKutyaAdapter
+{
+    public class NevAdapter
+    {
+        string connString;
+        DataTable kutyanevadatok;
+        public DataView Kutyanevadatok { get { return kutyanevadatok.DefaultView; } }
+        SQLiteDataAdapter adapter;
+        SQLiteConnection conn;
+
+        public NevAdapter(string connstring)
+        {
+            connString = connstring;
+            conn =new SQLiteConnection(connString);
+            adapter = new SQLiteDataAdapter("", conn);
+
+            adapter.SelectCommand = new SQLiteCommand(conn);
+            adapter.SelectCommand.CommandText = "select * from kutyanevek";
+
+            adapter.InsertCommand = new SQLiteCommand(conn);
+            adapter.InsertCommand.CommandText = "insert into kutyanevek " +
+                "(kutyanev) " +
+                "values(@kutyanev)";
+            adapter.InsertCommand.Parameters.Add("@kutyanev", DbType.String, 0, "kutyanev");
+
+            adapter.UpdateCommand = new SQLiteCommand(conn);
+            adapter.UpdateCommand.CommandText = "update kutyanevek" +
+                " set kutyanev=@kutyanev " +
+                " where id=@id";
+
+        }
+    }
+}
