99aa372bb83ee699e7438bed16aac96777d0640a
szajberpiraty <szajberpiraty@gmail.com>
2021-01-27 16:55:45 +0100

    Migrációs példa

---
+++ EFAuto14IEMigration/EFAuto14IEMigration/Auto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFAuto14IEMigration
+{
+    [Table("auto")]
+    public class Auto
+    {
+        [Key]
+        public string Rendszam { get; set; }
+        [Required]
+        public string Gyartmany { get; set; }
+        [Required]
+        public string Tipus { get; set; }
+        [Required]
+        public int GyartasiEv { get; set; }
+
+    }
+}
