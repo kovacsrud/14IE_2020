a18d064d5550f2d79ce550408ff4a3c545134f2f
szajberpiraty <szajberpiraty@gmail.com>
2021-02-24 16:16:20 +0100

    Teszt Projekt

---
+++ Alapmuveletek/AlapmuveletTest/Alaptest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Alapmuveletek;
+
+namespace AlapmuveletTest
+{
+    [TestClass]
+    public class Alaptest
+    {
+        [TestMethod]
+        public void OsszeadasTest()
+        {
+            //arrange,act,assert
+            Alapmuvelet alapmuvelet = new Alapmuvelet();
+
+            //act
+            var sut = alapmuvelet.Osszeadas(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, sut);
+
+
+        }
+    }
+}
---
+++ Alapmuveletek/AlapmuveletTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AlapmuveletTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlapmuveletTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("641576da-4c7b-4810-b497-43110f5ee781")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
