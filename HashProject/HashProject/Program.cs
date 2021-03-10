85eb9b3591b2b6a1a2df7f95dbfa0c4d2112cb61
szajberpiraty <szajberpiraty@gmail.com>
2021-03-10 14:57:57 +0100

    Hash készítés start

---
+++ HashProject/HashProject/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace HashProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string szoveg = "Valami szöveg";
+
+            byte[] szovegbytes = new UTF8Encoding().GetBytes(szoveg);
+
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            var hash = md5.ComputeHash(szovegbytes);
+
+            //22E67B691BCC1BB1C806364FB660294E
+            //22e67b691bcc1bb1c806364fb660294e
+
+            StringBuilder hashstring = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                hashstring.Append(hash[i].ToString("x2"));
+            }
+
+            Console.WriteLine(hashstring.ToString().ToUpper());
+
+            byte[] fajladatok = File.ReadAllBytes("toyota.jpg");
+
+            byte[] fajlhash = md5.ComputeHash(fajladatok);
+
+            hashstring = new StringBuilder();
+
+            for (int i = 0; i < fajlhash.Length; i++)
+            {
+                hashstring.Append(fajlhash[i].ToString("x2"));
+            }
+
+            Console.WriteLine(hashstring.ToString());
+            //3c10c2eabdbda72710357ba04f8a2941
+            //3C10C2EABDBDA72710357BA04F8A2941
+
+            //Készítsünk egy class library-t, amely megvalósítja
+            //a hash készítést több algoritmus szerint is
+            //a class library-t azért készítjük, hogy bármilyen
+            //alkalmazással használható legyen
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HashProject/HashProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HashProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HashProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a6cf4c36-7f29-49b0-bdf5-106c63eea72d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
