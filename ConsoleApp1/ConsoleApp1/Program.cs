234b873fa6c5558259c94730a784beaee48ea495
szajberpiraty <szajberpiraty@gmail.com>
2021-03-17 16:04:00 +0100

    Felület teszt

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://tanlaktanya.taszi.hu";
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            driver.FindElement(By.LinkText("Belépés")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("username")).SendKeys("seleniumtest");
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("password")).SendKeys("a1234567"+Keys.Enter);
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Programozás")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Szoftverfejlesztő (14d13b)")).Click();
+
+            Thread.Sleep(10000);
+
+            driver.FindElement(By.LinkText("Kilépés")).Click();
+
+            Thread.Sleep(5000);
+            
+            driver.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
