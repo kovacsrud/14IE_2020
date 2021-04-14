afd7e1a62b2b911bfe23a4d91a72d1609d495f7a
szajberpiraty <szajberpiraty@gmail.com>
2021-04-14 15:20:15 +0200

    Adat betöltés

--- WpfGamesData/WpfGamesData/MainWindow.xaml.cs
+++ WpfGamesData/WpfGamesData/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,28 @@ namespace WpfGamesData
     /// </summary>
     public partial class MainWindow : Window
     {
+        GameData gameData;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog()==true)
+            {
+                try
+                {
+                    gameData = new GameData(dialog.FileName, ';', 1);
+                    Debug.WriteLine(gameData.Games.Count);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,"Hiba!",MessageBoxButton.OK,MessageBoxImage.Error);                    
+                }
+                
+            }
         }
     }
 }
